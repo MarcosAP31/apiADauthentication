522e2d525c31a03fe8a46c74f764d88bf02f40e5
MarcosAP31 <marcos313art@gmail.com>
2024-07-05 21:19:31 -0500

    dsadsad

--- build/authConfig.js
+++ build/authConfig.js
@@ -2,9 +2,9 @@
 const msal = require('@azure/msal-node');
 const msalConfig = {
     auth: {
-        clientId: process.env.AZURE_CLIENT_ID,
-        authority: process.env.AZURE_AUTHORITY,
-        clientSecret: process.env.AZURE_CLIENT_SECRET
+        clientId: '70ba3f7e-84f0-4d8c-a8e4-fb7358414dcd',
+        authority: 'https://login.microsoftonline.com/ba5f9d02-0ba8-4c7f-95ad-7ffdc88513da',
+        clientSecret: 'tLd8Q~Ig.T74O59KYJd9UseMN1Tuy5pGG454rb11'
     },
     system: {
         tokenRenewalOffsetSeconds: 300,
