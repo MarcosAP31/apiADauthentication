e3005e8155e5a91dee09b9f962ca4de90b73c5d9
Marcos Arteta <marcos@claimpay.net>
2024-07-08 09:05:23 -0500

    dsadasd

--- build/authConfig.js
+++ build/authConfig.js
@@ -2,9 +2,9 @@
 const msal = require('@azure/msal-node');
 const msalConfig = {
     auth: {
-        clientId: '70ba3f7e-84f0-4d8c-a8e4-fb7358414dcd',
+        clientId: '7ed08e68-1ecd-4348-8bea-26f409e16021',
         authority: 'https://login.microsoftonline.com/ba5f9d02-0ba8-4c7f-95ad-7ffdc88513da',
-        clientSecret: 'tLd8Q~Ig.T74O59KYJd9UseMN1Tuy5pGG454rb11'
+        clientSecret: 'AXh8Q~Xr9Wd.6_KNMuEKig2xlVXPOIparX-C2a3L'
     },
     system: {
         tokenRenewalOffsetSeconds: 300,
